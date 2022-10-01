fc00895b80af321ce9416977c39279290649f0a0
Brian Maher <bmaher91@gmail.com>
2022-10-01 06:19:47 -0600

    compile and cache glad to speed up builds

--- build.bat
+++ build.bat
@@ -1,9 +1,21 @@
 @echo off
 
-rem TODO: Compile glad.c separately
+rem ////////////////////////////////////////////////////////////////////////////////
+rem                                   Config
+rem ////////////////////////////////////////////////////////////////////////////////
+
+:startConfig
 
 set debugBuild=0
-set exeName=game.exe
+set cleanBuildCache=0
+
+:endConfig
+
+rem ////////////////////////////////////////////////////////////////////////////////
+rem                               Setup compiler
+rem ////////////////////////////////////////////////////////////////////////////////
+
+:startCompilerSetup
 
 call cl >nul 2>nul
 
@@ -19,21 +31,64 @@ if not %errorlevel% == 0 (
     exit /b 1
 )
 
-if exist build call rmdir /s /q build
-
-mkdir build
-
-set defines=
-set includes=/I include
-set sources=code\third_party\*.c code\*.c
-set libs=lib\glfw3dll.lib
-
 set flags=/nologo /W4 /wd5105 /std:c17
 if %debugBuild% == 1 (
     set flags=%flags% /Zi /Od
 ) else (
     set flags=%flags% /O2
 )
+
+:endCompilerSetup
+
+rem ////////////////////////////////////////////////////////////////////////////////
+rem                               Manage build cache
+rem ////////////////////////////////////////////////////////////////////////////////
+
+:startManageBuildCache
+
+if exist build call rmdir /s /q build
+
+mkdir build
+
+if %cleanBuildCache% == 1 (
+    if exist build_cache call rmdir /s /q build_cache
+)
+
+if not exist build_cache mkdir build_cache
+
+:endManageBuildCache
+
+rem ////////////////////////////////////////////////////////////////////////////////
+rem                                 Compile GLAD
+rem ////////////////////////////////////////////////////////////////////////////////
+
+:startCompileGlad
+
+if exist build_cache\glad.obj goto endCompileGlad
+
+echo COMPILE-INFO: Compiling GLAD. Future builds will be faster.
+
+set defines=
+set includes=/I include
+set sources=code\third_party\glad.c
+
+call cl /c %flags% %defines% %includes% %sources%
+
+move glad.obj build_cache
+
+:endCompileGlad
+
+rem ////////////////////////////////////////////////////////////////////////////////
+rem                                 Compile game
+rem ////////////////////////////////////////////////////////////////////////////////
+
+:startCompileGame
+
+set defines=
+set includes=/I include
+set sources=code\*.c build_cache\*.obj
+set libs=lib\glfw3dll.lib
+set exeName=game.exe
 
 copy lib\glfw3.dll build >nul 2>nul
 
@@ -50,3 +105,5 @@ if %errorlevel% == 0 (
         call build\%exeName%
     )
 )
+
+:endCompileGame
